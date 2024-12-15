34658a593d9f622f3066579202071033217d0c09
serhii.klunnyi <serhii.klunnyi@appliedmethods.biz>
2024-12-15 07:01:11 +0200

    first commit

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,12 +1,43 @@
 package com.optimagrowth.license.controller;
 
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
 @RequiredArgsConstructor
-@RequestMapping(value = "v1/organization/" +
-        "{organizationId}/license")
 public class LicenseController {
+
+    private final LicenseService licenseService;
+
+    @GetMapping("/{licenseId}")                  // @PathVariable - эта аннотация обозначает значение параметра из URL
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
+                                              @PathVariable("licenseId") String licenseId) {
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request,
+                                                @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    }
+
+    @DeleteMapping("/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId,
+                                                @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
 }
